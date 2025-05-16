7481a08ca642e621967c691c7d09ba6c3aabc033
Dodla. Sai kiran <74188801+Saikirandodla@users.noreply.github.com>
2025-05-16 14:57:21 +0530

    Update first.java

--- demo/src/demo/first.java
+++ demo/src/demo/first.java
@@ -1,5 +1,14 @@
 package demo;
 
 public class first {
+  int units = 200;
+		double billToPay=0;
+		if(units<100) {
+			billToPay=units*1.20;
+		}
+		else if(units<300) {
+			billToPay = 100*1.20+(units-100)*2;
+		}
+		System.out.println("The Electricity bil for "+units+" is:"+billToPay);
 
 }
